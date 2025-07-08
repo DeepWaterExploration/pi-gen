23831c8322e33cc8488f4296211ce0300bea24e0
jdszekeres <jdszekeres@gmail.com>
2025-07-08 10:36:05 -0700

    Add bridge install adapter script

--- stage2/04-dwe-install/00-run.sh
+++ stage2/04-dwe-install/00-run.sh
@@ -29,8 +29,9 @@ method=auto
 EOF
 
 chmod 600 "$file_path"
+DWE_VERSION=${DWE_VERSION:-latest}
 
 # Run the DWE OS install script
 on_chroot <<- \EOF
-    curl -s https://raw.githubusercontent.com/DeepwaterExploration/DWE_OS_2/main/install.sh | sudo bash -s
+    curl -s https://raw.githubusercontent.com/DeepwaterExploration/DWE_OS_2/main/install.sh | sudo bash -s -- $DWE_VERSION
 EOF
---
+++ stage2/05-bridge-adapter/00-run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+# Install the Bridge Adapter
+
+#run the script as root from now on
+sudo bash << EOF
+INSTALL_DIR=/opt/bridge-adapter
+
+mkdir -p "${INSTALL_DIR}"
+
+wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/main.py" -O "${INSTALL_DIR}/main.py"
+wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/requirements.txt" -O "${INSTALL_DIR}/requirements.txt"
+wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/NetworkManager.py" -O "${INSTALL_DIR}/NetworkManager.py"
+wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/run_with_env.sh" -O "${INSTALL_DIR}/run_with_env.sh"
+
+cd "${INSTALL_DIR}"
+
+python3 -m venv env
+. env/bin/activate
+pip install -r requirements.txt
+
+cat > /etc/systemd/system/bridge-adapter.service << SERVICE_EOF
+[Unit]
+Description=Web Based UVC Control Driver for the DeepWater Exploration exploreHD and HDCam.
+
+[Service]
+Type=simple
+TimeoutStartSec=0
+Restart=always
+ExecStart=${INSTALL_DIR}/run_with_env.sh
+
+[Install]
+WantedBy=multi-user.target
+SERVICE_EOF
+
+
+systemctl enable bridge-adapter
+systemctl start bridge-adapter
+
+EOF
