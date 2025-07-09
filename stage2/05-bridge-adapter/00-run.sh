d153b45f192270cec3803dab16089dba54153d19
jdszekeres <jdszekeres@gmail.com>
2025-07-09 10:01:07 -0700

    Update bridge adapter installation script for improved logging and permissions

--- stage2/05-bridge-adapter/00-run.sh
+++ stage2/05-bridge-adapter/00-run.sh
@@ -1,18 +1,22 @@
 #!/bin/bash -e
 # Install the Bridge Adapter
 
+echo "Starting bridge-adapter installation script"
+log "Installing Bridge Adapter"
+
 #run the script as root from now on
-sudo bash << EOF
+on_chroot << EOF
 INSTALL_DIR=/opt/bridge-adapter
+echo "Installing Bridge Adapter to \${INSTALL_DIR}"
+mkdir -p "\${INSTALL_DIR}"
+ls "\${INSTALL_DIR}"
 
-mkdir -p "${INSTALL_DIR}"
+wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/main.py" -O "\${INSTALL_DIR}/main.py"
+wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/requirements.txt" -O "\${INSTALL_DIR}/requirements.txt"
+wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/NetworkManager.py" -O "\${INSTALL_DIR}/NetworkManager.py"
+wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/run_with_env.sh" -O "\${INSTALL_DIR}/run_with_env.sh"
 
-wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/main.py" -O "${INSTALL_DIR}/main.py"
-wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/requirements.txt" -O "${INSTALL_DIR}/requirements.txt"
-wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/NetworkManager.py" -O "${INSTALL_DIR}/NetworkManager.py"
-wget "https://raw.githubusercontent.com/DeepWaterExploration/DWEOS-wifi-detector/refs/heads/main/run_with_env.sh" -O "${INSTALL_DIR}/run_with_env.sh"
-
-cd "${INSTALL_DIR}"
+cd "\${INSTALL_DIR}"
 
 python3 -m venv env
 . env/bin/activate
@@ -26,14 +30,14 @@ Description=Web Based UVC Control Driver for the DeepWater Exploration exploreHD
 Type=simple
 TimeoutStartSec=0
 Restart=always
-ExecStart=${INSTALL_DIR}/run_with_env.sh
+ExecStart=\${INSTALL_DIR}/run_with_env.sh
 
 [Install]
 WantedBy=multi-user.target
 SERVICE_EOF
 
-
 systemctl enable bridge-adapter
 systemctl start bridge-adapter
 
+echo "Bridge adapter installation completed"
 EOF
